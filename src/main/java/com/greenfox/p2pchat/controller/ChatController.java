23d6d68f8d0b00ce96a0383b7374339b5a3f0041
norbert barics <norbert.barics@gmail.com>
2017-05-17 10:45:32 +0200

    application properties updated

--- src/main/java/com/greenfox/p2pchat/controller/ChatController.java
+++ src/main/java/com/greenfox/p2pchat/controller/ChatController.java
@@ -1,8 +1,14 @@
 package com.greenfox.p2pchat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChatController {
+
+  @RequestMapping("/")
+  public String chat(){
+    return "index";
+  }
 
 }
