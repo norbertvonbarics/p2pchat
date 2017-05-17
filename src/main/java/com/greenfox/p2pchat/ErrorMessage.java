750d2e92e2632bb4724b5ba133abe06eba28a11b
norbert barics <norbert.barics@gmail.com>
2017-05-17 11:52:19 +0200

    updated controller with exception handling

---
+++ src/main/java/com/greenfox/p2pchat/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.greenfox.p2pchat;
+
+public class ErrorMessage
+{
+  String error;
+
+  public ErrorMessage(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+}
--- src/main/java/com/greenfox/p2pchat/controller/ChatController.java
+++ src/main/java/com/greenfox/p2pchat/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.greenfox.p2pchat.controller;
 
+import com.greenfox.p2pchat.ErrorMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +10,13 @@ public class ChatController {
 
   @RequestMapping("/")
   public String chat(){
+    System.out.println("MUUUUKODJ");
     return "index";
   }
 
+  @ExceptionHandler(Exception.class)
+  public ErrorMessage parameterMissing(Exception e) {
+    System.err.println(e.getMessage());
+    return new ErrorMessage("shit just hits the fan");
+  }
 }
