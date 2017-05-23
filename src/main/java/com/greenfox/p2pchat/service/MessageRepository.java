317a7bba317676bc5e369a50089d9e6aca02dce8
norbert barics <norbert.barics@gmail.com>
2017-05-23 21:43:08 +0200

    changed order by to asc order

--- src/main/java/com/greenfox/p2pchat/service/MessageRepository.java
+++ src/main/java/com/greenfox/p2pchat/service/MessageRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<UserMessage, Long> {
-  public List<UserMessage> findAllByOrderByTimestamp();
+  public List<UserMessage> findAllByOrderByTimestampAsc();
 }
